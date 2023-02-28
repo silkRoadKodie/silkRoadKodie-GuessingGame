763cb9bc7f49aa196300d1e893973ca38b160633
Kodie Kuiper <antonio.pennyman@hyperisland.se>
2023-02-28 00:26:36 +0000

    guessNumber

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,7 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+        return guessNumber == secretNumber;
     }
+    
 }
